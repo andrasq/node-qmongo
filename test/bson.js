3792cb51c6e0f87246ae5ea7e0863878530e7e31
Andras <andras@andrasq.com>
2019-03-19 21:29:17 -0400

    rename test-mongo.js to test/time-mongo, bring over bson.js from qbson

---
+++ test/bson.js
@@ -0,0 +1,55 @@
+/*
+ * shim to unify the apis of the various incarnations of the bson package
+ * Originally from the qbson package.
+ *
+ * Copyright (C) 2017-2019 Andras Radics
+ * Licensed under the Apache License, Version 2.0
+ */
+
+// NOTE: nyc breaks with "ReferenceError: cov_9nxqz8quf is not defined" if
+// this file is excluded with -x, so move it into test/ none of which is profiled.
+
+//var bson = require('bson-v0.3.2');
+//var bson = require('bson-v0.4.23');             // 15% faster than bson3
+//var bson = require('bson-v0.5.7');              // 11% faster than bson3
+//var bson = require('bson-v1.0.4');              // 11% faster than bson3
+var bson = require('bson');
+
+var ObjectID, serialize, deserialize;
+
+if (typeof bson == 'function') {
+    ObjectID = bson.ObjectID;
+    serialize = new bson().serialize;
+    deserialize = new bson().deserialize;
+}
+else if (typeof bson.serialize === 'function') {
+    ObjectID = bson.ObjectID;
+    serialize = bson.serialize;
+    deserialize = bson.deserialize;
+}
+else if (bson.BSONPure && typeof bson.BSONPure.BSON == 'function') {
+    var obj = new bson.BSONPure.BSON()
+    ObjectID = bson.ObjectID;
+    serialize = function(o) { return obj.serialize(o) };
+    deserialize = function(b) { return obj.deserialize(b) };
+}
+else {
+    ObjectID = bson.ObjectID;
+    serialize = bson.BSONPure.BSON.serialize;
+    deserialize = bson.BSONPure.BSON.deserialize;
+}
+
+module.exports = {
+    ObjectID: ObjectID,
+    serialize: serialize,
+    deserialize: deserialize,
+    Long: bson.Long,
+    Timestamp:bson.Timestamp,
+    MinKey: bson.MinKey,
+    MaxKey: bson.MaxKey,
+};
+
+
+//console.log("AR:", module.exports.ObjectID())
+//console.log("AR:", module.exports.serialize({a:1}))
+//console.log("AR:", module.exports.deserialize(module.exports.serialize({a:1})))
